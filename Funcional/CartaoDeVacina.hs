8c38864afd19fc36b4f99d1fa5cb9e2fafd56069
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-02-22 15:31:10 -0300

    adicionando constructors e get's

--- Funcional/CartaoDeVacina.hs
+++ Funcional/CartaoDeVacina.hs
@@ -34,9 +34,27 @@ data CartaoDeVacina = CartaoDeVacina{
 getAtributoCartaoDeVacina :: CartaoDeVacina -> String
 getAtributoCartaoDeVacina (CartaoDeVacina {Id = i, pacienteId = p, aplicadorId = a, vacinaId = v, dataDeAplicao = [d]}) = i++","++p ++","++a++","++v","++d 
 
+-- pensar em lógica de acessar vacina através do id salvo no cartão
+getDosesDeVacinas :: String -> [Vacina]
+getDosesDeVacinas Vacina {vacinas = v} = v
+
 
 
 ------------------------------ Gera_Cartao ------------------------------
+
+mapeiaIdCartao :: [Cartao] -> [(String, Cartao)]
+mapeiaIdCartao [] = []
+mapeiaIdCartao (id:ids) = (getCartaoId i, i) : mapeiaIdCartao cs
+
+adicionaDose :: [Paciente] -> String -> Vacina -> Maybe [Paciente]
+adicionaDose [] idPaciente novaDose = Nothing
+adicionaDose (Paciente {nomePaciente = n, idPaciente = id, dataDeAplicao = d, vacina = v}:cs) idPaciente novaDose
+    | id == idPaciente = Just ([Paciente n id d ([novaDose])] ++cs)
+    | otherwise = adicionaDose cs idPaciente novaDose
+
+getDosesTomadasToString :: [String] -> String
+getDosesTomadasToString [] = []
+getDosesTomadasToString (d:ds) = if lenght ds > 0 then d ++ "," ++ getVacinaIdToString ds else d ++ "\n" 
 
 
 ------------------------------ IO_Cartao ------------------------------
