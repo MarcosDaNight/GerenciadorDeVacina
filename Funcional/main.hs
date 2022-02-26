0730739de9d7bdac6190d465aad8545f11f3d0a5
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2022-02-26 09:58:36 -0300

    adicionando util, getters e vizualizacao em vacina

--- Funcional/main.hs
+++ Funcional/main.hs
@@ -1,11 +1,14 @@
 import Models.Vacina
 
+import System.Directory
+import System.IO
+
 main:: IO()
 main = do
-    vacina <- getLine
-    let pfizer = read vacina :: Vacina
-    putStrLn $ show pfizer
-    putStrLn $ show $ nome pfizer
-    putStrLn $ show $ prazo pfizer
-    putStrLn $ show $ doses pfizer
+    dataVacina <- openFile "../Data/Vacina.txt" ReadMode
+    vacina <- lines <$> hGetContents dataVacina
+    print vacina
+    
+    
+
 
