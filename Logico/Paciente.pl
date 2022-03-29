6b6661758855f029e872a6bd38a55091b93e37d2
pedrosqra <pedro.lima@ccc.ufcg.edu.br>
2022-03-29 11:56:13 -0300

    Fazendo toda a classe paciente funcional

--- Logico/Paciente.pl
+++ Logico/Paciente.pl
@@ -23,6 +23,21 @@ exibirPacientes(FilePath) :-
 		lerJSON(FilePath, T),
 		exibirPacientesAux(T).
 
+% Regras para listar um Paciente
+exibirPacienteAux([H|T], Id) :- H.id \= Id, exibirPacienteAux(T, Id).
+exibirPacienteAux([H|T], Id) :- H.id =:= Id, exibirPacienteString(H,Id).
+exibirPacienteString(H,Id) :- nl,
+    write("CPF:"), writeln(H.id), nl,
+	write("Nome:"), writeln(H.nomePaciente), nl,
+	write("Data de Nascimento:"), writeln(H.dataNascimento), nl,
+	write("Data de Vacinacao:"), writeln(H.dataVacinacao), nl,
+	write("Vacina:"), writeln(H.vacina), nl.
+
+
+exibirPaciente(FilePath, Id) :-
+		lerJSON(FilePath, T),
+		exibirPacienteAux(T, Id).
+
 % Criando representação em formato String de um Paciente em JSON
 pacienteToJSON(ID, Nome, DataNascimento, DataVacinacao, Vacina, Out) :-
 		swritef(Out, '{"id":"%w","nomePaciente":"%w","dataNascimento":"%w","dataVacinacao":"%w","vacina":"%w"}', [ID, Nome, DataNascimento, DataVacinacao, Vacina]).
@@ -34,17 +49,16 @@ pacientesToJSON([H|T], [X|Out]) :-
 		pacientesToJSON(T, Out).
 
 % Salvar em arquivo JSON
-salvarPaciente(FilePath, Nome, Nascimento, DataVacinacao, Vacina) :- 
-    id(ID), incrementa_id,
+salvarPaciente(FilePath, Cpf, Nome, Nascimento, DataVacinacao, Vacina) :- 
 		lerJSON(FilePath, File),
 		pacientesToJSON(File, ListaPacientesJSON),
-		pacienteToJSON(ID, Nome, Nascimento, DataVacinacao, Vacina, PacienteJSON),
+		pacienteToJSON(Cpf, Nome, Nascimento, DataVacinacao, Vacina, PacienteJSON),
 		append(ListaPacientesJSON, [PacienteJSON], Saida),
 		open(FilePath, write, Stream), write(Stream, Saida), close(Stream).
 
 % Mudando o nome de um Paciente 
 editarNomePacienteJSON([], _, _, []).
-editarNomePacienteJSON([H|T], H.id, Nome, [_{id:H.id, nome:Nome, dataNascimento:H.dataNascimento, dataVacinacao:H.dataVacinacao, vacina:H.vacina}|T]).
+editarNomePacienteJSON([H|T], H.id, Nome, [_{id:H.id, nomePaciente:Nome, dataNascimento:H.dataNascimento, dataVacinacao:H.dataVacinacao, vacina:H.vacina}|T]).
 editarNomePacienteJSON([H|T], Id, Nome, [H|Out]) :- 
 		editarNomePacienteJSON(T, Id, Nome, Out).
  
@@ -56,13 +70,25 @@ editarNomePaciente(FilePath, IdPaciente, NovoNome) :-
 
 % Adicionando Vacina ao paciente
 adicionaVacinaAoPacienteJSON([], _, _, []).
-adicionaVacinaAoPacienteJSON([H|T], H.id, Vacina, [_{id:H.id, nome:H.nomePaciente, dataNascimento:H.dataNascimento, dataVacinacao:H.dataVacinacao, vacina:Vacina}|T]).
+adicionaVacinaAoPacienteJSON([H|T], H.id, Vacina, [_{id:H.id, nomePaciente:H.nomePaciente, dataNascimento:H.dataNascimento, dataVacinacao:H.dataVacinacao, vacina:Vacina}|T]).
 adicionaVacinaAoPacienteJSON([H|T], Id, Vacina, [H|Out]) :- 
 		adicionaVacinaAoPacienteJSON(T, Id, Vacina, Out).
 
 adicionaVacinaAoPaciente(FilePath, IdPaciente, Vacina) :-
 		lerJSON(FilePath, File),
 		adicionaVacinaAoPacienteJSON(File, IdPaciente, Vacina, SaidaParcial),
+		pacientesToJSON(SaidaParcial, Saida),
+		open(FilePath, write, Stream), write(Stream, Saida), close(Stream).
+
+% Adicionando Vacina a lista de vacinas do paciente (só conceito, não funciona ainda)
+adcVacinaAoPacienteJSON([], _, _, []).
+adcVacinaAoPacienteJSON([H|T], H.id, Vacina, [_{id:H.id, nomePaciente:H.nomePaciente, dataNascimento:H.dataNascimento, dataVacinacao:H.dataVacinacao, vacina:Vacina}|T]).
+adcVacinaAoPacienteJSON([H|T], Id, Vacina) :- write('cu'), write(T), write(Vacina), Id =:= H.id.
+
+
+adcVacinaAoPaciente(FilePath, IdPaciente, Vacina) :-
+		lerJSON(FilePath, File),
+		adcVacinaAoPacienteJSON(File, IdPaciente, Vacina, SaidaParcial),
 		pacientesToJSON(SaidaParcial, Saida),
 		open(FilePath, write, Stream), write(Stream, Saida), close(Stream).
 
